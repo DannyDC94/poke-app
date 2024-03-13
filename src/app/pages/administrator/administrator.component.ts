912d0a47a198757390e5460529be6216e4815980
DannyDC <daniel.canarejo@easysoft.com.ec>
2024-03-13 02:23:12 -0500

    Load data pokemon

--- src/app/pages/administrator/administrator.component.ts
+++ src/app/pages/administrator/administrator.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from '../../services/pokemon.service';
+import {FormControl} from "@angular/forms";
+import {debounceTime, distinctUntilChanged} from "rxjs/operators";
 
 @Component({
   selector: 'app-administrator',
@@ -7,17 +9,48 @@ import { PokemonService } from '../../services/pokemon.service';
   styleUrls: ['./administrator.component.scss']
 })
 export class AdministratorComponent implements OnInit {
+  loadData = false;
+  pokemonData: any[] = [];
+  filteredPokemons: any[]  = [];
+  selectedPokemons: any[]  = [];
+  searchControl = new FormControl('');
 
   constructor(private svcPokemon: PokemonService) { }
 
   ngOnInit(): void {
+    this.loadData = false;
     this.svcPokemon.getPokemons().subscribe({
-      next: value => {
-        console.log(value);
+      next: pokemonData => {
+        pokemonData.forEach((pokemonResponse) => {
+          this.pokemonData = pokemonResponse;
+          this.filteredPokemons = pokemonResponse.slice(0, 9);
+          console.log(this.pokemonData);
+          this.loadData = true;
+        })
       },
       error: err => {},
-      complete: () => {}
-    })
+      complete: () => {
+
+      }
+    });
+    this.searchControl.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged()
+    ).subscribe(searchTerm => {
+      this.filterPokemons(searchTerm);
+    });
+  }
+
+  getPokemon(pokemon: any) {
+    console.log(pokemon);
+    this.selectedPokemons.push(pokemon);
+    console.log(this.selectedPokemons);
+  }
+
+  filterPokemons(searchTerm: string): void {
+    this.filteredPokemons = this.pokemonData.filter(pokemon =>
+      pokemon['name'].toLowerCase().includes(searchTerm.toLowerCase())
+    ).slice(0, 9);
   }
 
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {forkJoin, Observable, of} from 'rxjs';
+import {catchError, map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,24 @@ export class PokemonService {
 
   constructor(private http: HttpClient) { }
 
-  getPokemons(): Observable<any> {
-    return this.http.get<any>('https://pokeapi.co/api/v2/');
+  getPokemons(): Observable<any[]> {
+    // @ts-ignore
+    return this.http.get<any>('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0')
+      .pipe(
+        map((response: any) => {
+          if (response && response.results) {
+            const pokemonObservables = response.results.map((pokemon: any) => {
+              return this.http.get<any>(pokemon.url);
+            });
+            return forkJoin(pokemonObservables);
+          }
+          return [];
+        }),
+        catchError((error: any) => {
+          console.error('Error al obtener datos de Pokémon:', error);
+          return of([]);
+        })
+      );
   }
+
 }
